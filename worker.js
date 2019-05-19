bc93d3e1771bc29f677cae7686f1af880b6538c8
Urban Dzindzinovic <dzindzinovic@yahoo.com>
2019-05-19 13:21:58 +0200

    Initial commit

---
+++ worker.js
@@ -0,0 +1,49 @@
+/* global self, caches, fetch */
+'use strict'
+
+var cachename = 'organize-my.life'
+var urlstocache = [
+  'index.css',
+  'index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  'https://use.fontawesome.com/releases/v5.8.1/css/all.css'
+];
+
+// install/cache page assets
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cachename)
+      .then(function (cache) {
+        console.log('cache opened')
+        return cache.addAll(urlstocache)
+      })
+  )
+})
+
+// intercept page requests
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // serve requests from cache (if found)
+      return response || fetch(event.request)
+    })
+  )
+})
+
+// service worker activated, remove outdated cache
+self.addEventListener('activate', function (event) {
+  console.log('worker activated')
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) {
+          // filter old versioned keys
+          return key !== cachename
+        }).map(function (key) {
+          return caches.delete(key)
+        })
+      )
+    })
+  )
+})
